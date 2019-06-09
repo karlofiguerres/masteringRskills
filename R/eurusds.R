c76ad2bd276c3103a0d8d1d5ded108abd18208ce
karlofiguerres <Figuerres_Karlo@student.ceu.edu>
2019-06-09 17:34:26 +0200

    fixed eurusds and added examples

--- R/eurusds.R
+++ R/eurusds.R
@@ -1,14 +1,17 @@
 #' Takes two arguments, date_from and date_to, and returns a data.table object on the daily exchange rates from date_from to date_to
 #' @param date_from starting date of our daily exchange rates
 #' @param date_to ending date of our daily exchange rates
-#' @return data.table ojbect
+#' @return data.table object
 #' @export
 #' @importFrom httr GET content
 #' @importFrom logger log_info
 #' @importFrom data.table data.table
+#' @examples
+#' eurusds('2019-01-01', '2019-01-15')
+#' eurusds('2019-01-01', '2019-01-30')
 eurusds <- function(date_from, date_to) {
   response <- GET(
-    'https://api.exchangeratesapi.io/latest?',
+    'https://api.exchangeratesapi.io/history',
     query = list(
       start_at = date_from,
       end_at   = date_to,
@@ -17,8 +20,9 @@ eurusds <- function(date_from, date_to) {
     ))
   exchange_rates <- content(response)
   usdeur <- exchange_rates$rates
-  log_info('Daily exchange rate of USD to EUR from {date_from} to {date_to} is:')
+  #log_info('Daily exchange rate of USD to EUR from {date_from} to {date_to} is:')
   usdeur <- data.table(
-    date = as.Date(names(exchange_rates)), usdeur = as.numeric(unlist(exchange_rates))
+    date = as.Date(names(usdeur)), usdeur = as.numeric(unlist(usdeur))
   )
+  usdeur
 }
