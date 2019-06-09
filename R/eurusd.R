82d0ffab6811543571f4f333090030cf90fd1807
Karlo Figuerres <Figuerres_Karlo@student.ceu.edu>
2019-06-09 16:27:45 +0200

    Base file with the first function, eurusd, working correctly

---
+++ R/eurusd.R
@@ -0,0 +1,19 @@
+#' Looks up the most recent USD to EUR exchange rate via an API and returns the rate as a number
+#' @return number
+#' @export
+#' @importFrom httr GET content
+#' @importFrom logger log_info
+#' @examples
+#' eurusd()
+eurusd <- function() {
+  GET(
+   'https://api.exchangeratesapi.io/latest?',
+    query = list(
+      base     = 'USD',
+      symbols  = 'EUR'
+    ))
+  exchange_rates <- content(response)
+  usdeur <- exchange_rates$rates$EUR
+  log_info('1 US Dollar is currently equivalent to {usdeur} Euros')
+  usdeur
+}
